bdc025ca947d1e11389dcddca5e54ef7fddd92c5
jglobe <shrkss0@gmail.com>
2023-01-22 19:00:35 +0300

    [web] Left only one alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-const path = require('path');
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path  from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@pages': path.join(__dirname, 'src/pages'),
-      '@components': path.join(__dirname, 'src/components'),
+      '@': path.join(__dirname, 'src'),
     },
   },
 })
